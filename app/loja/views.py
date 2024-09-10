bacdc6bbf708a86dc1c7b7f7295738d5ad3064ff
dudaa28 <eduardaq2805@gmail.com>
2024-09-10 01:08:13 -0300

    mudando configuracoes de rota e adicionando o carrinho ao usuario

--- app/loja/views.py
+++ app/loja/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from .models import Bolo, Profile
+import json
 
+
+@login_required
 def home(request):
     return render(request, 'home.html')
 
@@ -38,11 +45,35 @@ def cadastro(request):
             messages.error(request, "As senhas não coincidem")
     return render(request, 'cadastro.html')
 
+@login_required
 def catalogo(request):
     return render(request, 'catalogo.html')
 
+@login_required
 def basket(request):
     return render(request, 'basket.html')
 
+@login_required
 def admin(request):
     return render(request, 'admin.html')
+
+@require_POST
+def adicionar_ao_carrinho(request):
+    data = json.loads(request.body)
+    bolo_nome = data['bolo_nome']
+    tamanho = data['tamanho']
+    
+    # Encontra o bolo pelo nome
+    bolo = get_object_or_404(Bolo, sabor=bolo_nome)
+    
+    # Obtém o perfil do usuário
+    profile = Profile.objects.get(user=request.user)
+    
+    # Adiciona o bolo ao carrinho
+    profile.adicionar_bolo_ao_carrinho(bolo, tamanho)
+    
+    return JsonResponse({'success': True})
+
+def listar_carrinho(request):
+    profile = Profile.objects.get(user=request.user)
+    return JsonResponse({'carrinho': profile.listar_carrinho()})
